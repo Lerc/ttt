0446c6715f5af62d616c6f634c8e0bf0f8ecf4c2
Neil Graham <lerc@screamingduck.com>
2021-09-21 11:40:22 +1200

    Transforms, blending, color remapping, svg paths

--- ttt.js
+++ ttt.js
@@ -50,23 +50,24 @@ ttt=(td, only_this_index=-1,stack_depth=0) => {
 
 	let random=makeRandom(1337);
 
-	var ss=(v,a=0,b=1,w=v*v*v*(v*(v*6-15)+10))=>(1.0-w)*a+(w*b); 
-  var positiveMod=(v,size,q=v%size)=>q<0?size-q:q;
+	let ss=(v,a=0,b=1,w=v*v*v*(v*(v*6-15)+10))=>(1.0-w)*a+(w*b); 
+  let positiveMod=(v,size,q=v%size)=>q<0?size-q:q;
+  let clamp = a=> a<0?0:a>1?1:a;
 
-  var makeOp= ()=>{
-    var state;
-    var push=v=>state.push(v);
-    var pop=()=>state.pop();
+  let  makeOp= ()=>{
+    let state;
+    let push=v=>state.push(v);
+    let pop=()=>state.pop();
     
-    var stackOp=(argc,fn)=>( ()=>push(fn.apply(null,state.splice(-argc,argc))) );
+    let stackOp=(argc,fn)=>()=>push(fn(...state.splice(-argc,argc))) ;
 
-    var bi= fn=>(()=>{var b=pop(); push(fn(pop(),b));});
-    var un= fn=>( ()=>push(fn(pop())) );
-    var p= v=>( ()=>push(v));
+    let bi= fn=>(()=>{let b=pop(); push(fn(pop(),b));});
+    let un= fn=>( ()=>push(fn(pop())) );
+    let p= v=>( ()=>push(v));
 
-    var pushStateVar=name=>(()=>push(state[name]));
+    let pushStateVar=name=>(()=>push(state[name]));
     
-    var ops={
+    let ops={
       "*": bi((a,b)=>a*b),    
       "/": bi((a,b)=>a/b),    
       "-": bi((a,b)=>a-b),
@@ -89,45 +90,44 @@ ttt=(td, only_this_index=-1,stack_depth=0) => {
       "~": un(M.abs),
       "#": un(M.round),
       "$": un(M.floor),
+      "C": un(clamp),
       "%": stackOp(2,positiveMod),
       "!": un(x=>1-x),
       "?": un(x=>x<=0?0:1),
-      ":": ()=> {var a=pop(), b=pop(); state.push(a,b)},
-      ";": ()=> {var a=pop(), b=pop(), c=pop(); state.push(a,b,c)},
-      "d": ()=> {var a=pop(); state.push(a,a)}
+      ":": ()=> state.push(pop(),pop()),
+      ";": ()=> state.push(pop(),pop(),pop()),
+      "d": ()=> {let a=pop(); state.push(a,a)}
     }
     
-    for (var v of "tuvxyz") ops[v]=pushStateVar(v);
-    for (var i=0; i<10;ops[""+i]=p(i++));
+    for (let v of "tuvxyz") ops[v]=pushStateVar(v);
+    for (let i=0; i<10;ops[""+i]=p(i++));
 
     return (programState,opcode)=>{ state=programState; ops[opcode](); };
   }
 
-  var program =code=>{
-      var op=makeOp();
+  let program =code=>{
+		let op=makeOp();
       return (x,y,state = [])=>{
         state.x=x;
         state.y=y;
-        for (var c of code) op(state,c);
+        for (let c of code) op(state,c);
         return state.pop();
       }
   }
-  var perlin=(x,y,wrapX=256,wrapY=wrapX)=> {    
-    
-
-    var dg=(ix,iy,gi=random(positiveMod(iy,wrapY)*wrapX+positiveMod(ix,wrapX)*2)*M.PI*2)  =>
+  let perlin=(x,y,wrapX=256,wrapY=wrapX)=> {    
+    let dg=(ix,iy,gi=random(positiveMod(iy,wrapY)*wrapX+positiveMod(ix,wrapX)*2)*M.PI*2)  =>
             ((x-ix)*M.sin(gi)) + ((y-iy)*M.cos(gi));
-
-    var u=0|x;
-    var v=0|y;
-    var sx=x-u; 
-    var sy=y-v;
-    var u1=(u+1);
-    var v1=(v+1);
+		let u=0|x;
+    let v=0|y;
+    let sx=x-u; 
+    let sy=y-v;
+    let u1=u+1;
+    let v1=v+1;
     return ss(sy,ss(sx,dg(u,v),dg(u1,v)),ss(sx,dg(u,v1),dg(u1,v1)));
   }
 
-  var wrapPerlin=(x,y,wrapX=2,wrapY=wrapX)=>perlin(x*wrapX,y*wrapY,wrapX,wrapY);
+  let wrapPerlin=(x,y,wrapX=2,wrapY=wrapX)=>perlin(x*wrapX,y*wrapY,wrapX,wrapY);
+
 
 
 	return td.filter((d,i) => only_this_index < 0 || i == only_this_index).map(d => {
@@ -137,37 +137,47 @@ ttt=(td, only_this_index=-1,stack_depth=0) => {
 			c = e.getContext('2d'),
 			rgba_from_2byte = c => 
 				("#"+(c|65536).toString(16).slice(-4)),
-			
-				
-			fill_rect = (x, y, w, h, color) => {
-				c.fillStyle = rgba_from_2byte(color);
-				c.fillRect(x, y, w, h);
-			},
 
-			emboss_rect= (x, y, w, h, ...colors)	=> {
-				colors.map((color, j) => {
-					fill_rect(x+offsets[j], y+offsets[j], w, h, color);
+			emboss_rect= (x, y, w, h, top,bottom,fill)	=> {
+				[top,bottom,fill].map((color, j) => {
+					c.fillStyle = rgba_from_2byte(color);
+					c.fillRect(x+offsets[j], y+offsets[j], w, h)
 				})
 			}
 
 		// Set up canvas width and height
 		let W = e.width = d[i++];
 		let H = e.height = d[i++];
-
+		let paletteFromCode = code => {
+			let paletteFn=program(code);
+			let paletteEntry = (i,a=[0,0.5,1]) => a.map( p=>clamp(paletteFn(i/255,p) )*255|0 );
+			let palette = []; for (i=0;i<256;i++) palette.push(paletteEntry(i))  
+			return palette;
+		}
+		
+		let remap = (palette,setAlpha) => {
+			let image=c.getImageData(0,0,W,H);
+			let d=image.data;
+			for (let i=0; i< d.length;i+=4) {    
+				d.set(palette[d[i]*0.299 + d[i+1]*0.587 + d[i+2]*0.114|0],i);
+				d[i+3]=setAlpha||d[i+3];
+			}
+			c.putImageData(image,0,0);
+		}
 		// Fill with background color
-		fill_rect(0, 0, W, H, d[i++]);
+		emboss_rect(0, 0, W, H, 0,0, d[i++]);
 
+		
 		// Perform all the steps for this texture
 		while (i < d.length) {
 			let f = [
 				// 0 - rectangle: x, y, width, height, top, bottom, fill
-				(x, y, width, height, top, bottom, fill) => {
-					emboss_rect(x, y, width, height, top, bottom, fill)
-				},
+				emboss_rect,
 				
 				// 1 - rectangle_multiple: start_x, start_y, width, height, 
 				//                         inc_x, inc_y, top, bottom, fill
 				(sx, sy, w, h, inc_x, inc_y, top, bottom, fill) => {
+					//for (let x=sx, y=sy; y<H ;x=x+inc_x<W?x+inc_x:0,y+=x?0:inc_x) {
 					for (let x = sx; x < W; x += inc_x) {
 						for (let y = sy; y < H; y += inc_y) {
 							emboss_rect(x, y, w, h, top, bottom, fill);
@@ -182,7 +192,7 @@ ttt=(td, only_this_index=-1,stack_depth=0) => {
 							// Take the color value (first 3 nibbles) and 
 							// randomize the alpha value (last nibble)
 							// between 0 and the input alpha.
-							fill_rect(x, y, size, size, (color&0xfff0) + random()*(color&15));
+							emboss_rect(x, y, size, size, 0,0,(color&0xfff0) + random()*(color&15));
 						}
 					}
 				},
@@ -205,22 +215,47 @@ ttt=(td, only_this_index=-1,stack_depth=0) => {
 					);
 					c.globalAlpha = 1;
 				},
+				// 5 - previous texture transformed
+				(texture_index, x, y, hskew, vskew, hscale, vscale,  alpha, operation) => {
+					c.save();
 
-				// 5 - generate a stackie pattern
+					c.globalAlpha = alpha/15;
+					c.setTransform(hscale,vskew,hskew,vscale,x,y);
+					c.globalCompositeOperation=["source-over","lighter","multiply","screen","overlay","difference", "hue","saturation", "color","luminosity"][operation];
+					(
+						texture_index < td.length && stack_depth < 16 &&
+						c.drawImage(ttt(td, texture_index, stack_depth+1)[0], 0,0)
+					);
+					c.restore();
+				},
+				// 6 - generate a stackie pattern
 				(colorA,colorB,code) => {
 					let vm=program(code)
 					for (let x = 0; x < W; x += 1) {
 						for (let y = 0; y < H; y += 1) {
-							fill_rect(x, y, 1, 1, colorA);
-							let l = vm(x/W,y/W);							
-							c.globalAlpha = l<0?0:l>1?1:l
-							fill_rect(x, y, 1, 1, colorB);
+							emboss_rect(x, y, 1, 1,0,0, colorA);
+							c.globalAlpha=clamp(vm(x/W,y/H));							
+							emboss_rect(x, y, 1, 1,0,0, colorB);
 							c.globalAlpha = 1;
 						}
 					}
 				},
-				// 6 - Change the seed for the random number generator
-				seed => {random = makeRandom(seed)}
+				// 7 - Change the seed for the random number generator
+				seed => random = makeRandom(seed),
+
+				// 8 - sets all pixels to a palette colour indexed by current pixel brightness
+				(colourCode,setAlpha)=> remap( paletteFromCode(colourCode),setAlpha),
+
+				// 9 - SVG path shape 
+				(path,fillColor,strokeColor,lineWidth) => {
+					let p=new Path2D(path);
+					c.fillStyle = rgba_from_2byte(fillColor);
+					c.strokeStyle = rgba_from_2byte(strokeColor);
+					c.lineWidth = lineWidth;
+					c.fill(p);
+					c.stroke(p);
+				},
+
 			][d[i++]];
 			f(...d.slice(i, i+=f.length));
 		}
